5ad4f136c18ff640279a3988ca72dfff2d08644b
Emanuel3k <emanuelsantos_677@hotmail.com>
2024-01-08 17:26:57 -0300

    feat: implementation of email sending methods

--- api/email-processor/src/main/kotlin/br/com/emanuel3k/controller/EmailController.kt
+++ api/email-processor/src/main/kotlin/br/com/emanuel3k/controller/EmailController.kt
@@ -1,22 +1,24 @@
 package br.com.emanuel3k.controller
 
-import br.com.emanuel3k.model.Email
-import com.google.gson.Gson
-import io.smallrye.reactive.messaging.annotations.Blocking
+import br.com.emanuel3k.service.EmailService
+import io.smallrye.mutiny.Uni
 import io.vertx.core.json.JsonObject
 import jakarta.enterprise.context.ApplicationScoped
+import jakarta.inject.Inject
 import org.eclipse.microprofile.reactive.messaging.Incoming
 import org.eclipse.microprofile.reactive.messaging.Outgoing
+
 
 @ApplicationScoped
 class EmailController {
 
+    @Inject
+    private lateinit var emailService: EmailService
+
     @Incoming("requests")
     @Outgoing("emails")
-    @Blocking
-    fun process(event: JsonObject): Long {
-        Thread.sleep(8000)
-        val eventEmail = Gson().fromJson(event.toString(), Email::class.java)
-        return eventEmail.id
+    fun process(event: JsonObject): Uni<Long> {
+        Thread.sleep(5000)
+        return emailService.sendEmail(event)
     }
 }
