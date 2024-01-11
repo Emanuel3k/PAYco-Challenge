f3bd19d0d1e27625c21eac5b8c0795ec03c4f9d9
Emanuel3k <emanuelsantos_677@hotmail.com>
2024-01-10 22:43:20 -0300

    fix: long ID for UUID and resources moved to service layer

--- api/email-processor/src/main/kotlin/br/com/emanuel3k/service/EmailService.kt
+++ api/email-processor/src/main/kotlin/br/com/emanuel3k/service/EmailService.kt
@@ -7,19 +7,20 @@ import io.quarkus.mailer.reactive.ReactiveMailer
 import io.smallrye.mutiny.Uni
 import io.vertx.core.json.JsonObject
 import jakarta.enterprise.context.ApplicationScoped
-import jakarta.inject.Inject
-import org.eclipse.microprofile.config.inject.ConfigProperty
+import org.eclipse.microprofile.reactive.messaging.Incoming
+import org.eclipse.microprofile.reactive.messaging.Outgoing
+import java.util.*
 
 @ApplicationScoped
-class EmailService {
+class EmailService(
+    private val mailer: ReactiveMailer,
+) {
 
-    @Inject
-    private lateinit var mailer: ReactiveMailer
 
-    fun sendEmail(event: JsonObject): Uni<Long> {
+    @Incoming("requests")
+    @Outgoing("emails")
+    fun sendEmail(event: JsonObject): Uni<UUID> {
         val eventEmail = Gson().fromJson(event.toString(), Email::class.java)
-
-        eventEmail.sender
 
         return mailer.send(
             Mail.withText(
